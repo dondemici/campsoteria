7f40fa39f6077f76e6b4c98b1fce560ad824ed95
Dundee <86846098+dondemici@users.noreply.github.com>
2024-07-23 17:45:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,6 @@ function showSlides() {
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
 
-
-
 setInterval(runClock, 1000); // Use function reference directly, not as a string
 runClock();
 
@@ -23,4 +21,21 @@ function runClock(){
     let cDate = today.toLocaleDateString(); // Corrected method name
     let timeStr = today.toLocaleTimeString();
     document.getElementById("dateNow").innerHTML = cDate +" " + timeStr; // Use innerHTML for line breaks
+}
+
+let emailBody = document.getElementById("conMessage").value;
+
+function sendEmail() {
+  Email.send({
+      Host: "smtp.gmail.com",
+      Username: "acyantune@gmail.com",
+      Password: "Enter your password",
+      To: 'receiver@email_address.com',
+      From: "sender@email_address.com",
+      Subject: "Sending Email using javascript",
+      Body: emailBody,
+  })
+      .then(function (message) {
+          alert("mail sent successfully")
+      });
 }
